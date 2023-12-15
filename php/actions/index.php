354aed4b8f880acdb22a454922a89f08193e90af
MaxK0 <maxkoryakov1@gmail.com>
2023-12-15 16:17:28 +0500

    Добавление комментариев

--- php/actions/index.php
+++ php/actions/index.php
@@ -2,6 +2,7 @@
 
 require_once '../helpers.php';
 
+//Получение данных с формы, если нет, то null.
 $name = $_POST['name'] ?? null;
 $surname = $_POST['surname'] ?? null;
 $lastname = $_POST['lastname'] ?? null;
@@ -11,7 +12,7 @@ $email = $_POST['email'] ?? null;
 $telegramPhone = $_POST['telegram_phone'] ?? null;
 $telegramNick = $_POST['telegram_nick'] ?? null;
 
-//Добавление данных в сессию для последующего извлечения в input, если форма не пройдет валидацию
+// Добавление значений переменных в сессию для последующего изъятия.
 addOldValue(key: 'name', value: $name);
 addOldValue(key: 'surname', value: $surname);
 addOldValue(key: 'lastname', value: $lastname);
@@ -21,64 +22,88 @@ addOldValue(key: 'email', value: $email);
 addOldValue(key: 'telegram_phone', value: $telegramPhone);
 addOldValue(key: 'telegram_nick', value: $telegramNick);
 
-//Валидация
-$_SESSION['validation'] = [];
 
+// Валидация
+
+// Проверка имени на пустоту и длину.
 if (empty($name) || strlen($name) > 45) {
     addValidationError(fieldName: 'name', message: 'Неверное имя');
 }
 
+// Проверка фамилии на пустоту и длину.
 if (empty($surname) || strlen($surname) > 45) {
     addValidationError(fieldName: 'surname', message: 'Неверная фамилия');
 }
 
+// Проверка имени на длину. Это поле может быть пустым, т.к. отчество не обязательно.
 if (!empty($lastname) && strlen($lastname) > 45) {
     addValidationError(fieldName: 'lastname', message: 'Неверное отчество');
 }
 
+// Проверка даты выпуска на пустоту и, чтобы она была не больше, чем текущая дата.
+// Можно ограничить эту дату и по минимально возможному значению, если нужно.
 if (empty($releaseDate) || $releaseDate > date('YYYY-mm-dd')) {
     addValidationError(fieldName: 'release_date', message: 'Недопустимая дата');
 }
 
+// Проверка даты истечения на пустоту и, чтобы она была не меньше, чем текущая дата.
+// Можно ограничить эту дату и по максимально возможному значению, если нужно.
 if (empty($expiryDate) || $expiryDate < date('YYYY-mm-dd')) {
     addValidationError(fieldName: 'expiry_date', message: 'Недопустимая дата');
 }
 
+// Проверка на то, чтобы был указан хотя бы один контакт для уведомлений.
 if (empty($email) && empty($telegramPhone) && empty($telegramNick)) {
     addValidationError(fieldName: 'contact', message: 'Не указан ни один контакт');
 }
 
+// Если указана почта, то проверка по встроенному фильтру.
 if (!empty($email) && !filter_var(value: $email, filter: FILTER_VALIDATE_EMAIL)) {
     addValidationError(fieldName: 'email', message: 'Указана неверная почта');
 }
 
+// Если указан телефон, то проверка, чтобы количество знаков было только 11 и это были цифры.
 if (!empty($telegramPhone) && !preg_match('/^\d{11}$/', $telegramPhone)) {
     addValidationError(fieldName: 'telegram_phone', message: 'Неверный номер телефона');
 }
 
+// Если указан никнейм, то проверка, чтобы ник начинался только с буквы, а дальше от 3 до 31 знака либо буквы, либо цифры, либо _ с любым регистром.
 if (!empty($telegramNick) && !preg_match('/^[a-z]{1}[a-z\d_]{3,31}$/i', $telegramNick)) {
     addValidationError(fieldName: 'telegram_nick', message: 'Неверный ник');
 }
 
+// Если обнаружены неправильные поля, то снова пользователя отправляют заполнять данные, не давая идти программе дальше.
 if (!empty($_SESSION['validation'])) {
     redirect('/layout/index.php');
 }
 
+// Присваивание null к необязательным полям, если пустые.
+// Нужно, т.к. прошлые значения содеражт "", а не null.
+// В итоге, в БД будут пустые значения, а нужно null.
 if (empty($lastname)) $lastname = null;
 if (empty($email)) $email = null;
 if (empty($telegramPhone)) $telegramPhone = null;
 if (empty($telegramNick)) $telegramNick = null;
 
-//Добавление данных в БД
+
+
+// Добавление данных в БД.
+
+
+
+// Получение экземпляра БД.
 $pdo = getPDO();
 
-$userId = isDataExists(pdo: $pdo, table: 'user', elementDB: 'surname', elementProgram: $surname);
-$emailId = isDataExists(pdo: $pdo, table: 'email', elementDB: 'email', elementProgram: $email);
+// Получение id, если в таблицах уже есть вводимые данные, иначе null.
+$userId = getIdIfDataExists(pdo: $pdo, table: 'user', elementDB: 'surname', elementProgram: $surname);
+$emailId = getIdIfDataExists(pdo: $pdo, table: 'email', elementDB: 'email', elementProgram: $email);
+
 $telegramId = null;
+if (!empty($telegramPhone)) $telegramId = getIdIfDataExists(pdo: $pdo, table: 'telegram', elementDB: 'phone', elementProgram: $telegramPhone);
+if (!$telegramId && !empty($telegramNick)) $telegramId = getIdIfDataExists(pdo: $pdo, table: 'telegram', elementDB: 'nick', elementProgram: $telegramNick);
 
-if (!empty($telegramPhone)) $telegramId = isDataExists(pdo: $pdo, table: 'telegram', elementDB: 'phone', elementProgram: $telegramPhone);
-if (!$telegramId && !empty($telegramNick)) $telegramId = isDataExists(pdo: $pdo, table: 'telegram', elementDB: 'nick', elementProgram: $telegramNick);
 
+// Добавление пользователя, если его нет в таблице.
 if (!$userId) {
     $query = "INSERT INTO users (name, surname, lastname) VALUES (:name, :surname, :lastname)";
     $params = [
@@ -89,6 +114,7 @@ if (!$userId) {
     $userId = addData($pdo, $query, $params);
 }
 
+// Добавление почты, если указана и нет в таблице
 if (!empty($email) && !$emailId) {
     $query = "INSERT INTO emails (email) VALUES (:email)";
     $params = [
@@ -97,6 +123,7 @@ if (!empty($email) && !$emailId) {
     $emailId = addData($pdo, $query, $params);
 }
 
+// Добавление телеграмм контактов, если хотя бы один указан и нет в таблице
 if ((!empty($telegramNick) || !empty($telegramPhone)) && !$telegramId) {
     $query = "INSERT INTO telegrams (phone, nick) VALUES (:phone, :nick)";
     $params = [
@@ -106,6 +133,7 @@ if ((!empty($telegramNick) || !empty($telegramPhone)) && !$telegramId) {
     $telegramId = addData($pdo, $query, $params);
 }
 
+// Добавление данных в общую таблицу, если нужные id получены.
 if (!empty($userId) && (!empty($emailId) || !empty($telegramId))) {
     $query = "INSERT INTO signatures (user, release_date, expiry_date, email, telegram) VALUES (:user, :release_date, :expiry_date, :email, :telegram)";
     $params = [
@@ -117,9 +145,9 @@ if (!empty($userId) && (!empty($emailId) || !empty($telegramId))) {
     ];
     addData($pdo, $query, $params);
 
-    addValidationError(fieldName: 'submit', message: 'Информация сохранена');
-    $_SESSION['old'] = [];
+    addValidationError(fieldName: 'submit', message: 'Информация сохранена'); // Добавление в сессию для отображения, что информация сохранилась.
+    $_SESSION['old'] = []; // Очищение прошлых значений в полях.
 }
 
-redirect('/layout/index.php');
+redirect('/layout/index.php'); // Перенаправление обратно на странцицу ввода данных.
 
--- php/helpers.php
+++ php/helpers.php
@@ -4,39 +4,55 @@ session_start();
 
 require_once 'config.php';
 
+// Перенаправление на выбранную страницу.
 function redirect(string $path) {
     header("Location: $path");
     die();
 }
 
-function validationErrorAttr(string $fieldName) {
-    echo isset($_SESSION['validation'][$fieldName]) ? 'aria-invalid="true"' : '';
+// Добавление поля в сессию непройденной валидации вместе с сообщением: по какой причине не прошло. 
+function addValidationError(string $fieldName, string $message) {
+    $_SESSION['validation'][$fieldName] = $message;    
 }
 
+// Проверка на то, есть ли у поля ошибки.
 function hasValidationError($fieldName) : bool {
     return isset($_SESSION['validation'][$fieldName]);
 }
 
+// вывод ошибки в html и удаление из сессии непройденной валидации.
+// Удаление нужно для того, чтобы, если пользователь введёт данные правильно после того, как он это сделал неправильно,
+// то поле перестанет выделяться как ошибочное.
 function validationErrorMessage(string $fieldName) {
     $message = $_SESSION['validation'][$fieldName] ?? '';
     unset($_SESSION['validation'][$fieldName]);
     echo $message; 
 }
 
-function addValidationError(string $fieldName, string $message) {
-    $_SESSION['validation'][$fieldName] = $message;    
+// Добавление свойства aria-invalid="true", если поле не прошло валидацию.
+// Свойство нужно для того, чтобы выделить поле визуально.
+function validationErrorAttr(string $fieldName) {
+    echo isset($_SESSION['validation'][$fieldName]) ? 'aria-invalid="true"' : '';
 }
 
+// Добавление данных в сессию для последующего извлечения в input, если форма не пройдет валидацию.
+// Это нужно, так как страница с формой перезагружается.
 function addOldValue(string $key, mixed $value) {
     $_SESSION['old'][$key] = $value;
 }
 
+// Возвращает прошлое значение данных и сразу удаляет, т.к. больше не нужно.
 function old(string $key) : string {
     $value = $_SESSION['old'][$key] ?? '';
     unset($_SESSION['old'][$key]);
     return $value;
 }
 
+
+// Работа с БД
+
+
+// Получение экземпляра БД.
 function getPDO() : PDO {
     try {
         return new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';charset=utf8;dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
@@ -46,30 +62,35 @@ function getPDO() : PDO {
     }
 }
 
+// Добавление данных в таблицу.
 function addData(PDO $pdo, string $query, array $params) : int {
-    $stmt = $pdo->prepare($query);
+    $stmt = $pdo->prepare($query); // Приготовление к выполнению запроса.
 
     try {
-        $stmt->execute($params);
-        return $pdo->lastInsertId();
+        $stmt->execute($params); // Выполнение запроса. $params - массив, где ключ - название поля в БД, а значение - значение, которое пользователь ввел.
+        return $pdo->lastInsertId(); // Возвращение id вставленной строчки, чтобы потом его вставить в общую таблицу.
     }
     catch (PDOException $e) {
         die($e->getMessage());
     }
 }
 
-function isDataExists(PDO $pdo, string $table, string $elementDB, $elementProgram) : mixed {
+// Если введенные значения уже существуют в таблице, то возвращается id этого поля.
+function getIdIfDataExists(PDO $pdo, string $table, string $elementDB, $elementProgram) : mixed {
     try {
         $query = "SELECT * FROM {$table}s";
         $stmt = $pdo->query($query);
+
         while ($row = $stmt->fetch()) {
             if ($row[$elementDB] == $elementProgram) {
+                // У фамилии дополнительная проверка на совпадение имени и отчества.
                 if ($elementDB == 'surname') {
                     if ($row['name'] == $_POST['name'] && $row['lastname'] == $_POST['lastname']) return $row[$table . "_id"];
                 }
                 else return $row[$table . "_id"]; 
             }               
         }
+
         return null;
     }
     catch (PDOException $e) {
