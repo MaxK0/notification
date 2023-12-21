5b2d1f7ed09d2a3288f21a028fc25d057ef7ecf8
MaxK0 <maxkoryakov1@gmail.com>
2023-12-22 03:29:56 +0500

    Готовый бот для получения chatId пользователей

--- Bot/functions.php
+++ Bot/functions.php
@@ -10,21 +10,21 @@ function getPDO(array $dbConfig) : PDO {
 }
 
 function sendMessage(string $url, int $chatId, string $text) {
+    // Инициализация запроса.
     $con = curl_init();
-    //Формируем строку запроса – отправка пользователю сообщения «hello»
+
+    // Формируем строку запроса – отправка пользователю сообщения.
     $msg=$url."sendMessage?chat_id=".$chatId."&text=$text";
-    //Настраиваем запрос
+
+    // Настраиваем запрос.
     curl_setopt($con, CURLOPT_URL, $msg);
     curl_setopt($con, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($con, CURLOPT_HEADER, 0);
-    //Выполняем запрос
-    $output = curl_exec($con);
-    //Закрываем запрос
+
+    // Выполняем запрос.
+    curl_exec($con);
+
+    // Закрываем запрос.
     curl_close($con);
-    // $apiUrl = "https://api.telegram.org/bot$botToken/sendMessage";
-    //     $params = [
-    //         'chatId' => $chatId,
-    //         'text' => $text
-    //     ];
-    //     file_get_contents($apiUrl . '?' . http_build_query($params));
+    
 }
--- Bot/index.php
+++ Bot/index.php
@@ -5,28 +5,28 @@ $config = require_once 'config.php';
 
 $pdo = getPDO($config['db']);
 
-
-// Токен вашего бота
 $botToken = '6748061151:AAEeAYx13C62g7kX0C34_2IBxkHsaVLGXJw';
 $url="https://api.telegram.org/bot$botToken/";
 
-// Получаем данные из входящего запроса
+// Получаем данные из входящего запроса.
 $update = json_decode(file_get_contents("php://input"), true);
 
 if (isset($update['message'])) {
+    // Если отправлено сообщение, то вводим данные в переменные.
     $message = $update['message'];
     $chatId = $message['chat']['id'];
     $text = $message['text'];
     $username = $message['from']['username'];
-    // Проверяем, что получено сообщение "/start"
+
+    // Проверяем, что получено сообщение "/start".
     if ($text == '/start') {
-        // Проверяем, есть ли пользователь с таким ником в базе данных
+        // Проверяем, есть ли пользователь с таким ником в БД.
         $stmt = $pdo->prepare("SELECT * FROM telegrams WHERE nick = :nick");
         $params = ['nick' => $username];
         $stmt->execute($params);
 
-        // Если пользователь найден, обновляем его chatId
-        if (($row = $stmt->fetch(PDO::FETCH_ASSOC)) && empty($row['chat_id'])) {
+        // Если пользователь найден и chatId у него пустой, вставляем в БД его chatId.
+        if (($row = $stmt->fetch()) && empty($row['chat_id'])) {
             $tgId = $row['telegram_id'];
             $stmt = $pdo->prepare("UPDATE telegrams SET chat_id = :chatId WHERE telegram_id = :tgId");
             $params = [
